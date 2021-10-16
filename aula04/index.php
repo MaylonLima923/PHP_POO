f78a77d9f57a689186337c76d395710b778ed779
Maylon Lima <maylonlima923@gmail.com>
2021-10-16 05:02:57 -0300

    Arquivos aula 04

---
+++ aula04/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Métodos Getter, Setter e Constructors</title>
+</head>
+<body>
+    <?php
+        require_once 'Caneta.php';
+
+        $c1 = new Caneta("Bic", "Vermelha", 1.0);
+
+        print_r($c1);
+    ?>
+</body>
+</html>
